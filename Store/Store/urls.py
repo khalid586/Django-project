a202a2710b059b9eec4c2bdea4d723639310087c
khalid <khalidcomilla58@gmail.com>
2023-06-02 18:09:41 +0600

    admin updated

--- Store/Store/urls.py
+++ Store/Store/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "Kazi departmental store admin panel"
+admin.site.site_title = "Admin panel" 
+admin.site.index_title =  "Inventory Report"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls'))
