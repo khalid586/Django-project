239e28e7bb984312cc02c00e8d86b41ad11249e2
khalid <khalidcomilla58@gmail.com>
2023-06-02 12:50:04 +0600

    about and services pages added

--- Store/home/urls.py
+++ Store/home/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("", views.index, name = 'home')
+    path("", views.index, name = 'home'),
+    path("about", views.about, name = 'about'),
+    path("services", views.services, name = 'services')
 
 ]
--- Store/home/views.py
+++ Store/home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("this is homepage")
+
+def about(request):
+    return HttpResponse("this is about")
+
+def services(request):
+    return HttpResponse("this is services")
